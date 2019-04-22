97d67ab46a8c650ea81d33bc539e878b497b414a
iamlira <474948760@qq.com>
2019-04-23 00:52:19 +0800

    run on tomcat

--- src/main/java/org/hdu/Test.java
+++ src/main/java/org/hdu/Test.java
@@ -1,4 +1,22 @@
 package org.hdu;
 
-public class Test {
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/DIDI")
+public class Test extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("DIDIstribution");
+    }
 }
