1673d59f67334d78a4a70004a48c96bd7b5664ed
iamlira <474948760@qq.com>
2019-04-27 15:50:16 +0800

    almost done.add paper & update paper to go.

--- src/main/java/org/service/impl/PaperServcieImpl.java
+++ src/main/java/org/service/impl/PaperServcieImpl.java
@@ -4,9 +4,11 @@ import org.dao.PaperDAO;
 import org.pojo.Paper;
 import org.service.PaperService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PaperServcieImpl implements PaperService {
     @Autowired
     PaperDAO paperDao;
