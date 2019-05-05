9999e131213fb6acf1971dfb3996a538f62eac43
iamlira <474948760@qq.com>
2019-05-06 02:04:16 +0800

    add aop

--- src/main/java/org/controller/UserController.java
+++ src/main/java/org/controller/UserController.java
@@ -1,6 +1,9 @@
 package org.controller;
 
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.pojo.User;
 import org.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Aspect
 @Controller
 public class UserController {
     @Autowired
@@ -17,6 +21,7 @@ public class UserController {
     public ModelAndView login(User user) {
         ModelAndView mv=new ModelAndView();
         User queryUser = userService.queryByUser(user);
+        login_aop();
         if (user.getName().equals(queryUser.getName()) && user.getPwd().equals(queryUser.getPwd()))
             mv.setViewName("redirect:/paper/allPaper");
         else {
@@ -24,5 +29,15 @@ public class UserController {
             mv.setViewName("redirect:/");
         }
         return mv;
+    }
+
+    @Pointcut("execution(public * org.controller.*.*(..))")
+    public void login_aop(){
+
+    }
+
+    @Before("login_aop()")
+    public void login_log(){
+        System.out.println("before login");
     }
 }
