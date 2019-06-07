b8796be168b749281b340220bf6d7ceb1c4b4290
iamlira <474948760@qq.com>
2019-06-07 18:41:24 +0800

    //

--- src/main/java/org/controller/UserController.java
+++ src/main/java/org/controller/UserController.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.pojo.User;
 import org.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
